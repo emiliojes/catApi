b4c406ec364a8c13b2cb8f3ee47f1c7acb4db740
Emilio Nuñez <emiliojes@gmail.com>
2025-02-23 16:14:19 -0500

    Adding Styles to the Cat app, and adding favorite button, but without function yet

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 console.log("Hello World")
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3'
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_IbM0E4tujUoIiJdtZwleFyDSrQe8Rx3TjYi7JtToqLH7qiCae2USnN1v2jcjR1R3'
 
-async function catReload() {
+async function catRandomReload() {
     const res = await fetch(API_URL)
     const data = await res.json();
 
     console.log(data)
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
-    const img3 = document.getElementById('img3');
     img1.src = data[0].url;
     img2.src = data[1].url;
-    img3.src = data[2].url;
 
 
     //.catch (error => console.error('There is a error loading the image',error));
 }
 
-catReload();
+catRandomReload()

676b1217b7e4bf9187d636bd2575050c1e2e909b
Emilio Nuñez <emiliojes@gmail.com>
2025-02-23 13:45:24 -0500

    Now showing 3 images

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 console.log("Hello World")
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search'
-
-// document.getElementById('loadCat').addEventListener('click', () => {
-//     fetch(URL)
-//     .then(res => res.json())
-//     .then(data => {
-//         const img = document.querySelector('img');
-//         img.src = data[0].url;
-//     })
-//     .catch (error => console.error('There is a error loading the image',error));
-// });
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3'
 
 async function catReload() {
     const res = await fetch(API_URL)
     const data = await res.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+
+    console.log(data)
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+
+
     //.catch (error => console.error('There is a error loading the image',error));
 }
 
